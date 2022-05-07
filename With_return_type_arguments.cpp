28a54e998b5e9577cf76a0a000e77073a3b2276a
Prathiban Vinayagamoorthy <60544331+pattu1729@users.noreply.github.com>
2022-05-07 17:43:36 +0530

    Add files via upload

---
+++ With_return_type_arguments.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int add();
+int main()
+{
+add();
+return(c);
+}
+int add(int a,int b,int c)
+{
+printf("Enterthefirstnumber:");
+scanf("%d",&a);
+printf("Enterthesecondnumber:");
+scanf("%d",&b);
+c=a+b;
+printf("Thesum oftwonumbersis:%d",c);
+return(0);
+}
